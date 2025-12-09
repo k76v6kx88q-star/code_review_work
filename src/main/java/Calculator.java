113d16fd3d9ba616690e273dec7d6a81ae749b46
LeonidV05 <k76v6kx88q@privaterelay.appleid.com>
2025-12-09 14:04:53 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
     public int add(int a, int b){
-        return 0 
+        return 0;
     }
     public int dif(int a, int b){
-        return 0
+        return 0;
     }
     public int div(int a, int b){
-        return 0
+        return 0;
     }
     public int times(int a, int b){
-        return 0
+        return 0;
     }
     public int solver(){
-        return 0
+        return 0;
     }
 }
